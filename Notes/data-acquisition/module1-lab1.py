1e586535beb1c90a6744ce58be065f36c5e6a2ac
Frank Jansen <fphjansen@gmail.com>
2024-06-21 12:07:43 -0400

    initial data wrangling

--- Notes/data-acquisition/module1-lab1.py
+++ Notes/data-acquisition/module1-lab1.py
@@ -12,7 +12,7 @@ async def download(url, filename):
 
 file_path='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv'
 
-await download(file_path, "auto.csv")
+download(file_path, "auto.csv")
 file_name="auto.csv"
 
 df = pd.read_csv(file_name)
